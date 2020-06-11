763676d4adee90faa1bd9a184eb586cb2b8d4d6c
Joshua Cruz <joshua.cruz15@upr.edu>
2020-06-11 00:22:54 -0400

    Changed tabs to navbar, put navbar on top, navigation is now scrolldown

--- js/tab.js
+++ js/tab.js
@@ -1,23 +1,5 @@
-function openTab(evt, tabID) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabID).style.display = "block";
-    evt.currentTarget.className += " active";
+function goToByScroll(id) {
+    $('html,body').animate({
+        scrollTop: $("#" + id).offset().top -50
+    }, 'slow');
 }
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
